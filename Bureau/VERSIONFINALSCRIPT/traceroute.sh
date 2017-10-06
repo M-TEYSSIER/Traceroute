7683081c282533250a24906c92730b8e164ed64f
Maxime TEYSSIER <maxime.teyssier@laposte.net>
2017-10-06 18:02:08 +0200

    TracerouteV3

--- Bureau/VERSIONFINALSCRIPT/traceroute.sh
+++ Bureau/VERSIONFINALSCRIPT/traceroute.sh
@@ -18,20 +18,25 @@ if [ -z $1 ]
 		
 		exit -1
 	fi						      
-clear							      
+clear
+
+tshark -a duration:5 -Y icmp host $1 > test | ping $1 -c 3
+Moi="$(cat test| cut -c 4-| head -n 1 | cut -d " " -f 6)"
+echo $Moi
+rm test
 
 
 # La variable "IpCible" permet de definir dans cette variable la valeur indiqué dans la commande excecuté.
 IpCible=$1
 
 # La variable "Moi" à pour but d'afficher l'origine du le paquet.
-Moi="Moi -- "
+
 
 # La commande "NomFichier" permet de générer un ficher pour nom l'adresse IP ciblé en argument avec la spécificité ".route" pour xdot
 NomFichier="$(echo $IpCible | sed 's/\./\-/g').route"
 
 # Permet de rediriger le flux de la variable "Moi" dans le fichier généré par la variable "NomFichier".
-echo -n  "$Moi" > $NomFichier
+echo -n  '"'"$Moi"'"' "-- " > $NomFichier
 
 # Initialisation des variable "ttl", "Hop", "HopPlusUn".
 ttl=1
@@ -84,14 +89,14 @@ for ttl in $(seq 1 30)
 		do	
 			if [ "$ttl2" == "$ttl" ]
 			then
-				echo -n " $(cat $NomFichier |grep Moi | sed 's/ -- / -/g' | cut -d "-" -f $((($ttl2*2)+1)) )" >> test
+				echo -n " $(cat $NomFichier |grep $Moi | sed 's/ -- / -/g' | cut -d "-" -f $((($ttl2*2)+1)) )" >> test
 			else
 
-				echo -n " , $(cat $NomFichier |grep Moi | sed 's/ -- / -/g' |sed 's/;/ -/g' | cut -d "-" -f $((($ttl2*2)+1)) )" >> test
+				echo -n " , $(cat $NomFichier |grep $Moi | sed 's/ -- / -/g' |sed 's/;/ -/g' | cut -d "-" -f $((($ttl2*2)+1)) )" >> test
 		fi
 		done
 	echo -n " [shape=ellipse,fontcolor=white];" >> test
-	echo -n "$(cat test | sed -e 's/ /Moi /')" >> $NomFichier
+	echo -n "$(cat test | sed -e 's/ /"'$Moi'" /')" >> $NomFichier
 	echo ""	>> $NomFichier
-	echo "$(cat $NomFichier | sed 's/"[*]"/[65535]/g')" > $NomFichier
+	echo "$(cat $NomFichier | sed 's/'[*]'/[65535]/g')" > $NomFichier
 	rm test
