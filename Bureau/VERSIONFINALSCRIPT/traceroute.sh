cd43107f76f79122a594f25effa00998e48e6149
Maxime TEYSSIER <maxime.teyssier@laposte.net>
2017-10-06 16:55:49 +0200

    VersionV2

--- Bureau/VERSIONFINALSCRIPT/traceroute.sh
+++ Bureau/VERSIONFINALSCRIPT/traceroute.sh
@@ -1,28 +1,57 @@
 #!/bin/bash
 
-if [ -z $1 ]
-	then 
-		echo " Pas de paramètre "
-		exit -1	
-	fi
-clear
+#############################################################################################################
+# Description:												    #
+# Ce script a pour but de tracer le chemin d'un paquet et d'en créer un cartographie.			    #
+# 													    #
+# Le script pourra tracer la route du paquet avec differents protocoles et sur différents numéros de ports. #
+#													    #
+# Outils utilisés:  traceroute, xdot, cut, grep, sed.							    #
+# 													    #
+#############################################################################################################
 
+
+# Cette boucle permet de vérifier la présence d'un argument.  
+if [ -z $1 ]						      
+	then 						      
+		echo " Pas de paramètre "		      
+		
+		exit -1
+	fi						      
+clear							      
+
+
+# La variable "IpCible" permet de definir dans cette variable la valeur indiqué dans la commande excecuté.
 IpCible=$1
+
+# La variable "Moi" à pour but d'afficher l'origine du le paquet.
 Moi="Moi -- "
 
+# La commande "NomFichier" permet de générer un ficher pour nom l'adresse IP ciblé en argument avec la spécificité ".route" pour xdot
 NomFichier="$(echo $IpCible | sed 's/\./\-/g').route"
+
+# Permet de rediriger le flux de la variable "Moi" dans le fichier généré par la variable "NomFichier".
 echo -n  "$Moi" > $NomFichier
+
+# Initialisation des variable "ttl", "Hop", "HopPlusUn".
 ttl=1
 Hop=""
 HopPlusUn=""
-listArg=( "-U -p 1194" "-U -p 5060" "-U -p 5060" "-T -p 80" "-T -p 443" "-T -p 22"  "-I" )
 
-#listArg=("")
-# Premiere ligne 
-for ttl in $(seq 1 30) 
+# La liste "listArg" contient les differentes combinaisons de protocoles et ports.
+listArg=( "-I" "-U -p 1194" "-U -p 5060" "-T -p 80" "-T -p 443" "-T -p 22" )
+
+
+# Première ligne: affiche le parcours du paquet envoyer avec differents protocoles. Les adresses IP des sauts s'affichera tant que possible.
+
+#Boucle permettant de compter le nombre de "ttl" du paquet (qui sera limité à 30).
+for ttl in $(seq 1 30)  
 	do
+#		Boucle qui change de champs dans le tableau 'listArg', donc qui change les paramètres de protocoles et du n° de port.
 		for proto in "${listArg[@]}" 
 			do 
+#				La variable "Hop" permet de récuperer sur l'excecution de la commande traceroute, avec la valeur de "ttl", l'adresse IP du saut.
+#				
 				Hop=$(traceroute -f $ttl -m $ttl -q 1 $proto $IpCible -n |grep -v traceroute | cut -c 5- | cut -d " " -f 1  )
 				HopPlusUn=$(traceroute -A -f $(($ttl+1)) -m $(($ttl+1)) -q 1 $proto $IpCible -n |grep -v traceroute | cut -c 5- | cut -d " " -f 1,2  )
 			
@@ -30,15 +59,20 @@ for ttl in $(seq 1 30)
 				then   
 					echo -n  '"'" $Hop "'"'"; " >> $NomFichier
 					break 2
-				elif [ "$Hop" == "*" ] && [ "$proto" == "-I" ]
+				elif [ "$Hop" == "*" ] && [ "$proto" == "-I" ] && [ "$HopPlusUn" == "*" ]
+				then
+					echo -n " Inconnuedesaut$ttl" >> $NomFichier
+					echo -n " -- "'"' "Inconnue de saut $ttl" '"'" -- " >> $NomFichier
+					break
+				elif [ "$Hop" == "*" ] && [ "$proto" == "-I" ] && [ "$HopPlusUn" != "*" ]
 				then
 					echo -n " Inconnue " >> $NomFichier
-					echo -n " -- "'"' "Réseau avant $HopPlusUn & Réseau après $Hop"'"'" -- " >> $NomFichier
+					echo -n " -- "'"' "Réseau avant $HopPlusUn & saut $ttl" '"'" -- " >> $NomFichier
 					break
 				elif [ "$Hop" != "*" ] && [ "$Hop" != "$IpCible" ]
 				then
 					echo -n  '"'" $Hop "'"'  >> $NomFichier
-					echo -n " -- "'"' "Réseau avant $HopPlusUn & Réseau après $Hop" '"'" -- " >> $NomFichier
+					echo -n " -- "'"' "Réseau avant $HopPlusUn & saut $ttl" '"'" -- " >> $NomFichier
 					break
 				fi
 			done
@@ -59,4 +93,5 @@ for ttl in $(seq 1 30)
 	echo -n " [shape=ellipse,fontcolor=white];" >> test
 	echo -n "$(cat test | sed -e 's/ /Moi /')" >> $NomFichier
 	echo ""	>> $NomFichier
+	echo "$(cat $NomFichier | sed 's/"[*]"/[65535]/g')" > $NomFichier
 	rm test
